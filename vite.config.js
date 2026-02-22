50be26ac321f0cdf81d14c65da8b9e6f19a5ffff
eskandr5 <mohammadeskandr5@gmail.com>
2026-02-22 21:37:56 +0300

    Add gh-pages and homepage configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/social-page/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
