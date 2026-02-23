76e13b81ea015fe1875c5037ebf9096813a9465a
eskandr5 <mohammadeskandr5@gmail.com>
2026-02-23 15:20:36 +0300

    Update Rightbar with Follow component and custom delete modal

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -38,8 +38,9 @@ const Navbar = () => {
                     {/* Icons - Middle (Hidden on Mobile) */}
                     <div className="hidden md:flex items-center gap-8 text-gray-600">
                         <Link title="Home" to="/feed" className="hover:text-blue-600 transition"><AiOutlineHome size={24} /></Link>
-                        <Link title="Messages" className="hover:text-blue-600 transition"><AiOutlineMessage size={24} /></Link>
                         <Link title="Notifications" to="/notifications" className="hover:text-blue-600 transition"><AiOutlineBell size={24} /></Link>
+                        <Link title="Messages" className="hover:text-blue-600 transition"><AiOutlineMessage size={24} /></Link>
+
                     </div>
 
                     {/* Right Side: Profile & Logout */}
